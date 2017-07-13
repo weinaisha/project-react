7c9464fa48bbed7d4d97a5db52bd8f38ba87695f
weizhazha <1163504612@qq.com>
2017-07-13 21:00:44 +0800

    finish

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react'
 import './TodoItem.css'
-export default class TodoItem extends Component{
+export  default class TodoItem extends Component{
     render(){
         return(
             <div className="TodoItem">
